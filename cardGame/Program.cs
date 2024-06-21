fee78745d0ed1cf933991eb6b987136001ff065e
S-JOBI23-062\student <231049@st.yoshida-g.ac.jp>
2024-06-21 12:01:06 +0900

    プロジェクトの作成、コードの編集 #1

---
+++ cardGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardGame
+{
+    internal class Program
+    {
+        public static int[] cardNumerical;
+       
+
+        static void Main(string[] args)
+        {
+            string cardName;
+            int cardNum;
+            
+            for (int i = 0; i < 4; i++) 
+            {
+                Console.Write("{0}つめのカードの数値を入力してください>", i + 1,cardName=Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out cardNum);
+                Console.ReadLine();
+
+                cardNumerical[i] = cardNum;
+
+                Console.WriteLine("{0}つめの数値は{1}です", cardNumerical[i+1], cardNum);
+                Console.ReadLine();
+            }
+        }
+    }
+}
